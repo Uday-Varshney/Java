75fdf200df62518709461b9c5cac8096207ec636
Uday-Varshney <85391692+Uday-Varshney@users.noreply.github.com>
2022-03-13 22:57:13 +0530

    Update bubblesort.java

--- bubblesort.java
+++ bubblesort.java
@@ -2,12 +2,12 @@ class bubblesort{
     public void bubblesort(int arr[]){
         int n=arr.length;
         int temp;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n-1;j++){
-                if(arr[j]>arr[i]){
-                    temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
                 }
             }
         }
